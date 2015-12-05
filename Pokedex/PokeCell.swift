d77031fdd9d26956fd0e4fad8e43fd0858c747ca
Mehdi Yembou <mehdiyembou@MacBook-Pro-de-Mehdi.local>
2015-12-05 13:17:49 +0100

    create a custom UITableViewCell : PokeCell

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Mehdi Yembou on 05/12/2015.
+//  Copyright © 2015 Mehdi Yembou. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
