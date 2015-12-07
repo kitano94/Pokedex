69ffa2ab36f7e038eaa51025a40e36b024cc6cea
Mehdi Yembou <mehdiyembou@MacBook-Pro-de-Mehdi.local>
2015-12-07 11:58:01 +0100

    IBOutlet detail view controller and updating class Pokemon

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -11,6 +11,13 @@ import Foundation
 class Pokemon {
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _baseAttack: String!
+    private var _nextEvolutionTxt: String!
     
     var name: String {
         return _name
@@ -20,9 +27,44 @@ class Pokemon {
         return _pokedexId
     }
     
-    init(name: String, pokedexId: Int) {
+    var description: String {
+        return _description
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var defense: String {
+        return _defense
+    }
+    
+    var height: String {
+        return _height
+    }
+    
+    var weight: String {
+     return _weight
+    }
+    
+    var baseAttack: String {
+        return _baseAttack
+    }
+    
+    var nextEvolutionTxt: String {
+        return _nextEvolutionTxt
+    }
+    
+    init(name: String, pokedexId: Int, description: String, type: String, defense: String, height: String, weight: String, baseAttack: String, nextEvolutionTxt: String) {
         self._name = name
         self._pokedexId = pokedexId
+        self._description = description
+        self._type = type
+        self._defense = defense
+        self._height = height
+        self._weight = weight
+        self._baseAttack = baseAttack
+        self._nextEvolutionTxt = nextEvolutionTxt
     }
     
     
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
     
+    // MARK: Properties
     
     @IBOutlet weak var nameLbl: UILabel!
-    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttackLbl: UILabel!
+    @IBOutlet weak var nextEvoLbl: UILabel!
+    @IBOutlet weak var evo1Img: UIImageView!
+    @IBOutlet weak var evo2Img: UIImageView!
     
     var pokemon: Pokemon!
 
@@ -21,5 +32,12 @@ class PokemonDetailVC: UIViewController {
 
         nameLbl.text = pokemon.name
     }
+    
+    // MARK: Actions
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 }
